b3d39902e73dd5991d803a8576be55342d440197
hyojun9292 <hyojun99222@gmail.com>
2024-10-05 12:15:36 +0900

    인터셉팅 라우트 - 현재 i/flow/login 진입 시 모달은 띄워지나 메인 화면의 page가 아닌 i/flow/login 과 패러렐 라우팅으로 띄워짐 - i, login, page 는 children에 prop @modal/i/flow/login 은 modal prop - 이 때 children은 i/login/page 로 modal만 i/flow/login 으로 처리하는게 인터셉팅 라우트를 통해 가능 - (..i) 패키지를 만듬 (..) 이나 (.) 은 브라우저 주소 기준 상대경로 - i 를 대체해야 하므로 @modal 하위에 (.)i 를 만든다 - 클라이언트에서 라우팅 할 때만 인터셉터 라우팅이 적용된다 - 새로고침이나 첫 진입하는 경우 SSR 적용되어 i/flow/login 화면이 렌더링됨

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <h2>지금 가입하세요.</h2>
         <Link href={"/i/flow/singup"} className={styles.signup}>회원가입</Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href={"/login"} className={styles.login}>로그인</Link>
+        <Link href={"/i/flow/login"} className={styles.login}>로그인</Link>
       </div>
     </>
   );
