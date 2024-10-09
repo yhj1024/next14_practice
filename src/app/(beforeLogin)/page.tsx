17afa87ff94072fc83ecba5941782f8efaa8e724
hyojun9292 <hyojun99222@gmail.com>
2024-10-09 22:49:10 +0900

    - SinupModal 컴포넌트 추가 (LoginModal 과 동일한 방식) - i/flow/login 을 /login 으로 변경 이 전에 문제가 있던 원인은 server에서 redirect 하기 때문에 인터셉팅 라우팅이 정상적으로 적용이 안 되었기 때문  +이렇게 해도 안 나오는데 /login 이 빈 화면 컴포넌트라서 그렇다 따라서 로그인 컴포넌트 내용을 login 에도 그대로 적용

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요.</h2>
-        <Link href={"/i/flow/singup"} className={styles.signup}>회원가입</Link>
+        <Link href={"/i/flow/signup"} className={styles.signup}>회원가입</Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href={"/i/flow/login"} className={styles.login}>로그인</Link>
+        <Link href={"/login"} className={styles.login}>로그인</Link>
       </div>
     </>
   );
